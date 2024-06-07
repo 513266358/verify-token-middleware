9e67c784dab6ee1a2b8db18aef6b21fa68bedbe3
yexiaoping <demonyxp@vip.qq.com>
2024-06-07 11:43:09 +0800

    1

--- src/GetUser.php
+++ src/GetUser.php
@@ -11,24 +11,31 @@ class GetUser
     {
         $user = \JWTAuth::parseToken()->authenticate();
         if ($user == false) {
-            return ApiResponse::defineResponse(401,'未登录',[$user],200);
+            return ApiResponse::defineResponse(401, '未登录', [$user], 200);
         }
 
         $data = $request->all();
-        array_walk_recursive($data, function (&$input) {
+        array_walk_recursive(
+            $data, function (&$input) {
             $input = htmlspecialchars($input, ENT_HTML5);
-        });
+        }
+        );
         $request->merge($data);
 
-        $userInfo['openid']            = $user->OPENID;
-        $userInfo['user_id']           = $user->SYS_USER_ID;
-        $userInfo['unionid']           = $user->UNION_ID;
-        $userInfo['nick_name']         = $user->USER_NAME;
-        $userInfo['phone']             = $user->TELEPHONE;
-        $userInfo['file_user_id']      = $user->file_user_id;
-        $userInfo['id']                = $user->id;
-        $userInfo['sys_user_id']       = $user->SYS_USER_ID;
-        $userInfo['source']       = $user->USER_SOURCE;
+        $userInfo['openid']                      = $user->OPENID;
+        $userInfo['user_id']                     = $user->SYS_USER_ID;
+        $userInfo['unionid']                     = $user->UNION_ID;
+        $userInfo['nick_name']                   = $user->USER_NAME;
+        $userInfo['phone']                       = $user->TELEPHONE;
+        $userInfo['file_user_id']                = $user->file_user_id;
+        $userInfo['id']                          = $user->id;
+        $userInfo['sys_user_id']                 = $user->SYS_USER_ID;
+        $userInfo['source']                      = $user->USER_SOURCE;
+        $userInfo['is_distribution']             = $user->is_distribution;
+        $userInfo['distribution_time']           = $user->distribution_time;
+        $userInfo['pid']                         = $user->pid;
+        $userInfo['is_pid']                      = $user->is_pid;
+        $userInfo['distribution_is_place_order'] = $user->distribution_is_place_order;
         $request->attributes->add(['userInfo' => $userInfo]);
 
         return $next($request);
